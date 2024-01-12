3ddba454ca8126466b5be4fb9d68e2b13fe8ae53
Chromatic Vizier <Zjq8947@gmail.com>
2024-01-12 22:28:36 +0800

    Added Housing price forcast.

--- Housing_price_forcast.py
+++ Housing_price_forcast.py
@@ -7,3 +7,10 @@ import matplotlib.pyplot as plt
 
 (train_data, test_data), (train_labels, test_labels) = boston_housing.load_data()
 
+#data standardization
+mean = train_data.mean(axis=0)
+train_data -= mean
+std = train_data.std(axis=0)
+train_data /= std
+test_data -= mean
+test_data /= std
